119c95f4e072c09aaa9183d075166550c5ce261d
Marci <yu.gi.ohlover1984@gmail.com>
2022-01-28 13:39:01 -0600

    :recycle :bulb: :mute: Rename functions and remove console logs

--- mapper.py
+++ mapper.py
@@ -1,18 +1,18 @@
 # Author: Marci DeVaughan
 # Created: 26 Jan 2022
-# Updated: 26 Jan 2022
+# Updated: 28 Jan 2022
 # A simple map reduce program
 
-f = open("inputData.txt","r")  # open file, read-only
-o = open("outputData.txt", "w") # open file, write
-for line in f:  
+fi = open("inputData.txt","r")  # open file, read-only
+fo = open("mappedData.txt", "w") # open file, write
+for line in fi:  
     rowData = line.strip().split("    ") # DT: List of Lists
-    print (rowData )
-    print (len(rowData ))
+    # print (rowData )
+    # print (len(rowData))
     if len(rowData) == 6:
         date, time, location, dept, amount, payType = rowData
         #assign names
-        print ("{0}\t{1}".format(location, amount))
-        o.write("{0}\t{1}\n".format(location, amount))
-f.close()
-o.close()
+        # print ("{0}\t{1}".format(location, amount))
+        fo.write("{0}\t{1}\n".format(location, amount))
+fi.close()
+fo.close()
