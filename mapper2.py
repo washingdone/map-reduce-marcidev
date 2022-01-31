c69c22c05e17b4803df5122408cfc2148bda464b
Marci <yu.gi.ohlover1984@gmail.com>
2022-01-30 20:42:43 -0600

    :bug: update remapper to format numbers for easy sorting later on

--- mapper2.py
+++ mapper2.py
@@ -1,6 +1,6 @@
 # Author: Marci DeVaughan
 # Created: 29 Jan 2022
-# Updated: 29 Jan 2022
+# Updated: 30 Jan 2022
 # A simple map reduce program
 
 fi = open("outputData.txt","r")  # open file, read-only
@@ -9,6 +9,10 @@ for line in fi:
     rowData = line.strip().split("\t") # DT: List of Lists
     if len(rowData) == 2:
         location, amount = rowData #assign names
-        fo.write("{0}\t{1}\n".format(amount, location))
+        splitArr = amount.split(".",1) # split in two for trimming
+        splitArr[0] = splitArr[0].zfill(4) # pad the dollars side to 4 characters
+        splitArr[1] = splitArr[1][:2] # trim the cents side to 2 characters
+        formatAmount = splitArr[0]+"."+splitArr[1] # combine the reformatted parts back into proper format
+        fo.write(f"{formatAmount}\t{location}\n")
 fi.close()
 fo.close()
